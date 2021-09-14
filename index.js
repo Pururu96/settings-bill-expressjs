8a32f948d76db231e2013f9d2ac42b14b75f8f50
Pururu <pururumphahlele@gmail.com>
2021-09-14 08:28:40 +0200

    Second draft.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var SettingsBill = require('./settings')
 let app = express();
+const settingsBill = SettingsBill( )
 
 //set up view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// parse application/x-www-form-urlencoded
+// parse application/x -www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
@@ -25,11 +27,12 @@ app.get("/", function (req, res) {
 app.post('/settings', function (req, res) {
 
     settingsBill.setSettings({
-        callCost: req.body.smsCost,
-        smsCost: req.body.callCost,
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
         warningLevel: req.body.warningLevel,
         criticalLevel: req.body.criticalLevel
     })
+    console.log(settingsBill.getSettings());
     res.redirect('/')
 });
 
@@ -40,11 +43,12 @@ app.post('/action', function (req, res) {
 })
 
 app.get('/actions', function (req, res) {
-
+    res.render('actions', {actions: settingsBill.actions()})
 })
 
-app.get('/actions/:type', function (req, res) {
-
+app.get('/actions/:actionType', function (req, res) {
+    const actionType = req.params.actionType
+    res.render('actions', {actions: settingsBill.actionsFor(actionType)}) 
 })
 
 const PORT = process.env.PORT || 3009;
